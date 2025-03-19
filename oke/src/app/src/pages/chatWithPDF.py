2157b1708caaf436bfb64a64b4a2438ba25ec508
Marc Gueury <marc.gueury@oracle.com>
2025-03-19 09:34:48 +0100

    v3.0

--- oke/src/app/src/pages/chatWithPDF.py
+++ oke/src/app/src/pages/chatWithPDF.py
@@ -99,14 +99,15 @@ def get_conversation_chain(vector_store):
 
 # Function to handle user input and display the chat history
 def handle_user_input(question):
-    response = st.session_state.conversation({'question': question})
-    st.session_state.chat_history = response['chat_history']
+    if st.session_state.conversation: 
+        response = st.session_state.conversation({'question': question})
+        st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 # Main function to set up the Streamlit app
 def main():
